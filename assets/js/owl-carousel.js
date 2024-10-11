a537dfc9b37a1ced8f98ba19cf718553d0b443e4
Light Memory <info.lightmemory@gmail.com>
2024-10-11 19:09:10 +0200

    Big changes

--- assets/js/owl-carousel.js
+++ assets/js/owl-carousel.js
@@ -3471,3 +3471,27 @@ $(document).ready(function(){
 	  }
 	});
   });
+
+ 
+
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const answer = item.querySelector('.faq-answer');
+    item.addEventListener('click', () => {
+      item.classList.toggle('active');
+      if (item.classList.contains('active')) {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      } else {
+        answer.style.maxHeight = "0";
+      }
+
+      faqItems.forEach(otherItem => {
+        if (otherItem !== item) {
+          otherItem.classList.remove('active');
+          otherItem.querySelector('.faq-answer').style.maxHeight = "0";
+        }
+      })
+    })
+  })
+
